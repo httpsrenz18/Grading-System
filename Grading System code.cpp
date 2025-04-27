48587e2dcb0ca80c24da1949797d8b8b1dfcd214
http.renz <ramosreniel16@gmail.com>
2025-04-27 14:28:42 +0800

    Update Grading System code.cpp

--- Grading System code.cpp
+++ Grading System code.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+
 using namespace std;
 
 struct Student {
@@ -11,62 +12,71 @@ struct Student {
     string remark;
 };
 
-float inputFloat(string prompt) {
-    float val;
+// Limits for grades input, Enter Grades: 
+float gradeLimits(string prompt) {
+    float value;
     while (true) {
         cout << prompt;
-        if (cin >> val && val >= 0 && val <= 100) {
+        if (cin >> value && value >= 0 && value <= 100) {
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            return val;
+            return value;
         }
         cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Invalid. Enter number from 0 to 100.\n";
     }
 }
 
-int inputInt(string prompt, int min, int max) {
-    int val;
+// How many students you want to add, How many students? (1-30)
+int inputStudent(string prompt, int minimum, int max) {
+    int value;
     while (true) {
         cout << prompt;
-        if (cin >> val && val >= min && val <= max) {
+        if (cin >> value && value >= minimum && value <= max) {
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            return val;
+            return value;
         }
         cin.clear(); cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid. Enter number from " << min << " to " << max << ".\n";
+        cout << "Invalid. Enter number from " << minimum << " to " << max << ".\n";
     }
 }
 
+
+//If you prompt wrong to the question Y(yes) N(no) it will repeat
 char inputYN(string prompt) {
-    char c;
+    char command;
     while (true) {
         cout << prompt;
-        cin >> c; cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        c = toupper(c);
-        if (c == 'Y' || c == 'N') return c;
+        cin >> command; cin.ignore(numeric_limits<streamsize>::max(), '\n'); //only accept first letter of your input/answer
+        command = toupper(command); //converting the command in uppercase. y - Y and n - N
+        if (command == 'Y' || command == 'N') return command;
         cout << "Enter Y or N.\n";
     }
 }
 
 int main() {
     do {
-        int count = inputInt("How many students? (1-30): ", 1, 30);
+        // uses vector to stored list of students
+        int count = inputStudent("How many students? (1-30): ", 1, 30);
         vector<Student> students;
-
+        
+        
         for (int i = 0; i < count; ++i) {
             Student s;
             cout << "\n[Student " << i + 1 << "]\nEnter name: ";
-            getline(cin, s.name);
-            s.math = inputFloat("Math: ");
-            s.science = inputFloat("Science: ");
-            s.english = inputFloat("English: ");
+            getline(cin, s.name); //getline use to allows spaces
+            s.math = gradeLimits("Math: ");
+            s.science = gradeLimits("Science: ");
+            s.english = gradeLimits("English: ");
             s.average = (s.math + s.science + s.english) / 3;
-            s.remark = s.average >= 90 ? "Excellent" : s.average >= 75 ? "Passed" : "Failed";
-            students.push_back(s);
-
+            s.remark = s.average >= 90 ? "Excellent" : s.average >= 75 ? "Passed" : "Failed"; //? asking if the statement is true ':' if not
+            students.push_back(s); // pushback is vertors command, it will repeat the process untile vector is true
+            
+            
+            // asking if you want to summarized the grades early
             if (inputYN("Summarized Grades? (Y/N): ") == 'Y') break;
         }
-
+        
+        //sorting the grades from highest to lowest
         sort(students.begin(), students.end(), [](auto &a, auto &b) {
             return a.average > b.average;
         });
@@ -83,8 +93,8 @@ int main() {
                  << setw(10) << s.english << setw(10) << fixed << setprecision(1) << s.average
                  << setw(10) << s.remark << "\n";
         }
-    } while (inputYN("\nRepeat program? (Y/N): ") == 'Y');
+    } while (inputYN("\nRepeat program? (Y/N): ") == 'Y'); //asking if you wnat to repeat the program
 
-    cout << "\nThank you!\n";
+    cout << "\nThank you!\n"; //End of the Program
     return 0;
 }
